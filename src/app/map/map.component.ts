2c659d84ef7fdeca3e3821ac155146d6d27cee3d
Bradley Biehl <bradley.biehl@gmail.com>
2021-01-04 20:02:08 -0600

    leaflet: map working, but styling problems

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,6 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { Map as LeafletMap } from 'leaflet';
+import { AfterViewInit, Component, Input } from '@angular/core';
+import { Map as LeafletMap, TileLayer } from 'leaflet';
+import { Neighborhood } from '../models/neighborhood';
 
 @Component({
     selector: 'app-map',
@@ -7,9 +8,22 @@ import { Map as LeafletMap } from 'leaflet';
     styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements AfterViewInit {
-
+    @Input() isLoading!: boolean | null;
+    @Input() errors!: any | null;
+    @Input() neighborhoods!: Neighborhood[] | null;
     public leafletMap!: LeafletMap;
+    public tiles!: TileLayer;
 
     ngAfterViewInit(): void {
+        this.createMap();
+    }
+
+    private createMap(): void {
+        this.leafletMap = new LeafletMap('leafletMapId').setView([41.2, -95.95], 9);
+        this.tiles = new TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            maxZoom: 17,
+            minZoom: 6,
+        }).addTo(this.leafletMap);
     }
 }

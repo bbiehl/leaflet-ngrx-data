5297573764d339a575bda195e0b090e8170ecc9e
Bradley Biehl <bradley.biehl@gmail.com>
2021-01-09 08:23:42 -0600

    refactored map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -5,14 +5,20 @@ import { Neighborhood } from '../models/neighborhood';
 @Component({
     selector: 'app-map',
     templateUrl: './map.component.html',
-    styleUrls: ['./map.component.scss']
+    styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements AfterViewInit {
     @Input() isLoading!: boolean | null;
     @Input() errors!: any | null;
     @Input() neighborhoods!: Neighborhood[] | null;
     public leafletMap!: LeafletMap;
-    public tiles!: TileLayer;
+    private readonly openStreetMapUrl =
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    private readonly openStreetMapAttribution =
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    private readonly tiles = new TileLayer(this.openStreetMapUrl, {
+        attribution: this.openStreetMapAttribution,
+    });
 
     ngAfterViewInit(): void {
         this.createMap();
@@ -22,14 +28,12 @@ export class MapComponent implements AfterViewInit {
         this.leafletMap = new LeafletMap('leafletMapId', {
             center: {
                 lat: 41.2,
-                lng: -95.95
+                lng: -95.95,
             },
+            layers: [this.tiles],
             maxZoom: 17,
             minZoom: 6,
             zoom: 9,
         });
-        this.tiles = new TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        }).addTo(this.leafletMap);
     }
 }
